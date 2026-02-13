b88a9b5b3091bba5a7ef3a901778fbfa432f89b9
Wee Chee Hung <weechee700@gmail.com>
2026-02-13 21:04:00 +0800

    Deploy prebuilt Valentine site

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-// Serve the Vite build folder
+// Serve prebuilt files
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
